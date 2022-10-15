ad2d08f879d2bbd32599222050d4ab213697cbdf
Artur Okoński <okonski.a.dev@gmail.com>
2022-10-15 15:55:04 +0200

    Inject dependencies

--- lib/customers_data_downloader.rb
+++ lib/customers_data_downloader.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class CustomersDataDownloader
-  def download_customers_csv_data(file_path:)
-    payload = StripeClient.new.fetch_customers_data
-    CustomersDataSaver.new.save_customers_data(payload: payload, file_path: file_path)
+  def initialize(stripe_client: StripeClient.new, customers_data_saver: CustomersDataSaver.new)
+    @stripe_client = stripe_client
+    @customers_data_saver = customers_data_saver
   end
+
+  def download_customers_csv_data(file_path:)
+    payload = stripe_client.fetch_customers_data
+    customers_data_saver.save_customers_data(payload: payload, file_path: file_path)
+  end
+
+  private
+
+  attr_reader :stripe_client, :customers_data_saver
 end
